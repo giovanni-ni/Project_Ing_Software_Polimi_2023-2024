3e2aa62b1fc91c11739661d612fd19754a9abc0e
nuge77 <10801809@polimi.it>
2024-03-24 01:05:10 +0100

    correction

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/module/TargetDeck.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.module;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TargetDeck {
+
+    private ArrayList<TargetCard> targetDeck;
+    public TargetDeck(TargetCard [] targetCards){
+        targetDeck= new ArrayList<>();
+
+    }
+
+}
