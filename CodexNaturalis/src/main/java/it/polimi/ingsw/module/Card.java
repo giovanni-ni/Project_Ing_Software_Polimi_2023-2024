01dcc8bbaabd43472c5a153eeb880c98f06ba528
Jo <giovanni.ni@mail.polimi.it>
2024-03-26 02:48:01 +0100

    implementation of all Card Classes, add some new methods in Board class, not final version, improvements will be done later

--- CodexNaturalis/src/main/java/it/polimi/ingsw/module/Card.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/module/Card.java
@@ -5,26 +5,55 @@ import java.util.Map;
 
 public class Card {
 
-	private int Code;
-
+	private int code;
 	private boolean isFront;
-
 	public Map<CornerPosition, Elements> corners;
 
+	public Card() {
 
-	public boolean isGoldCard(){ /*those booleans would help if some code need to check the requirements that only gold card has*/
-		return false;
-	} //*watch enum CardType
-
-	public boolean isResourceCard(){
-		return false;
 	}
 
-	public boolean isInitialCard(){
-		return false;
+	public Card(int code, boolean isFront) {
+		this.code = code;
+		this.isFront = isFront;
+		/**/
 	}
 
-	public boolean isFront() {
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public boolean getIsFront() {
 		return isFront;
 	}
+
+	public void setFront(boolean front) {
+		isFront = front;
+	}
+
+	public Map<CornerPosition, Elements> getCorners() {
+		return corners;
+	}
+
+	public void setCorners(Map<CornerPosition, Elements> corners) {
+		this.corners = corners;
+	}
+
+	public boolean isGoldCard(Card c){ /*those booleans would help if some code need to check the requirements that only gold card has*/
+		return c instanceof GoldCard;
+	} //*watch enum CardType
+
+	public boolean isResourceCard(Card c){
+		return c instanceof ResourceCard;
+	}
+
+	public boolean isInitialCard(Card c){
+		return c instanceof InitialCard;
+	}
+
+
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/module/ResourceCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/module/ResourceCard.java
@@ -2,20 +2,36 @@ package it.polimi.ingsw.module;
 
 public class ResourceCard extends Card {
 
-	private Elements Kingdom;
+	private Elements kingdom;
 
 	private int basePoint;
 
-	public int getBasePoint() { /*added for board */
-		return basePoint;
+	public ResourceCard(){
+		super();
 	}
 
-	public void setKingdom() {
+	public ResourceCard(int code, boolean isFront) {
+		super(code, isFront);
+	}
 
+	public ResourceCard(int code, boolean isFront, Elements elem, int basePoint) {
+		super(code, isFront);
+		this.kingdom = elem;
+		this.basePoint = basePoint;
 	}
 
 	public Elements getKingdom() {
-		return null;
+		return this.kingdom;
+	}
+	public void setKingdom(Elements kingdom) {
+		this.kingdom = kingdom;
+	}
+
+	public int getBasePoint() { /*added for board */
+		return this.basePoint;
+	}
+	public void setBasePoint(int basePoint) {
+		this.basePoint = basePoint;
 	}
 
 	public int countPoint() {
