90c40b3ef751be0f6538a9b278ee2b4e9e3e21a1
Gong Marco <caijun.polimi@mail.polimi.it>
2024-05-21 01:51:29 +0200

    Gui scenes, start menu, easy join if need, AskNickname.fxml, add icon to scened

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/Gui/GUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/Gui/GUI.java
@@ -3,23 +3,28 @@ package it.polimi.ingsw.view.Gui;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
+import java.io.IOException;
+
+
 public class GUI extends Application {
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) throws IOException {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
-        loader.setRoot(new AnchorPane());
-        Parent root = loader.getRoot();
+        Image icon = new Image("icon.png");
+
         // Set the title of the stage
-        primaryStage.setTitle("FXML Example");
-
+        Parent root = FXMLLoader.load((getClass().getResource("AskNickname.fxml")));
+        primaryStage.setTitle("Codex Naturalis");
+        root.setStyle("-fx-background-color: black;");
+        primaryStage.getIcons().add(icon);
         // Set the scene with the loaded FXML
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root,1280,720));
 
+        primaryStage.setResizable(false);
         // Show the stage
         primaryStage.show();
 
