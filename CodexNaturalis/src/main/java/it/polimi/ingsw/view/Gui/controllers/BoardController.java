ec1ea533c6019254d2599b72b5fb08e85b74add1
nuge77 <10801809@polimi.it>
2024-05-22 01:34:18 +0200

    Board GUI

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/Gui/controllers/BoardController.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.view.Gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class BoardController {
+    @FXML
+    ImageView cardOnHandBackground, boardBrown, firstCardOnHand, secondCardOnHand, thirdCardOnHand;
+
+    public void displayImage()
+    {
+        Image myImage = new Image("boardBrown.png");
+        Image myImage2 =new Image("CardOnHandBackGround.png");
+        boardBrown.setImage(myImage);
+        cardOnHandBackground.setImage(myImage2);
+
+    }
+
+}
