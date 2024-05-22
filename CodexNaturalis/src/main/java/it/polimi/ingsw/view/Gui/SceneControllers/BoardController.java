b46168859f67773d9062fa3552b5d10136e4ca3d
nuge77 <10801809@polimi.it>
2024-05-22 16:07:27 +0200

    BoardController

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/Gui/SceneControllers/BoardController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/Gui/SceneControllers/BoardController.java
@@ -1,20 +1,64 @@
 package it.polimi.ingsw.view.Gui.SceneControllers;
 
+import it.polimi.ingsw.model.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class BoardController {
-    @FXML
-    ImageView cardOnHandBackground, boardBrown, firstCardOnHand, secondCardOnHand, thirdCardOnHand;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-    public void displayImage()
+public class BoardController {
+
+    private ArrayList<ResourceCard> resourceDeck;
+
+    private ArrayList<GoldCard> goldDeck;
+
+    private ArrayList<TargetCard> targetDeck;
+
+    private ArrayList<TargetCard> commonTarget;
+
+    @FXML
+    ImageView cardOnHandBackground, boardBrown, firstCardOnHand, secondCardOnHand, thirdCardOnHand,
+            deckBackground, pointTable;
+    @FXML
+    Label gameStatus;
+
+
+    public void setUpBoard()
     {
-        Image myImage = new Image("playGround.png");
-        Image myImage2 =new Image(".png");
-        boardBrown.setImage(myImage);
+        Image myImage1 = new Image("playGround.png");
+        Image myImage2 =new Image("transparentTabCoh.png");
+        Image myImage3 =new Image("transparentTabDeck.png");
+        Image myImage4 =new Image("pointTableBottom.png");
+
+        boardBrown.setImage(myImage1);
         cardOnHandBackground.setImage(myImage2);
+        deckBackground.setImage(myImage3);
+        pointTable.setImage(myImage4);
+    }
+
+
+
+    private void showCardOnHand(ArrayList<Card> cardOnHand){
+        for(Card c : cardOnHand) {
+            if(c.isGoldCard()) {
+                Image myImage5 = new Image("GoldCardFront (" + c.getCode() + ").png");
+            }
+            else {
+                Image myImage5 = new Image("ResourceCardFront (" + c.getCode() + ").png");
+            }
+        }
+    }
+    private void showDecks(ArrayList<GoldCard> goldDeck, ArrayList<ResourceCard> resourceDeck,
+                           ArrayList<TargetCard> commonTarget){
 
     }
+
+
+
 
 }
