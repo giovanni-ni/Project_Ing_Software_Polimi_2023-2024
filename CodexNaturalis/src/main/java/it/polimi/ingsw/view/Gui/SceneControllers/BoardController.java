dde0d8d33a1e19d7bbcddfef073137496afca806
nuge77 <10801809@polimi.it>
2024-05-24 01:56:11 +0200

    Gridpane and scroll pane

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/Gui/SceneControllers/BoardController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/Gui/SceneControllers/BoardController.java
@@ -2,16 +2,29 @@ package it.polimi.ingsw.view.Gui.SceneControllers;
 
 import it.polimi.ingsw.model.*;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BoardController extends GenericSceneController {
 
@@ -23,6 +36,12 @@ public class BoardController extends GenericSceneController {
 
     private ArrayList<TargetCard> commonTarget =new ArrayList<>();
 
+    private ArrayList<ImageView> cardsOnHand= new ArrayList<>();
+
+    private boolean isClickedCardOnHand=false;
+    private boolean isClickedBoard= false;
+    private boolean deckCardClicked= false;
+
     @FXML
     ImageView cardOnHandBackground, boardBrown, firstCardOnHand, secondCardOnHand, thirdCardOnHand,
             deckBackground, pointTable, firstResourceCard, secondResourceCard, kingdomResourceDeck,
@@ -30,7 +49,9 @@ public class BoardController extends GenericSceneController {
     @FXML
     Label gameStatus;
     @FXML
-    Button button;
+    Button button, playACard, getACard;
+    @FXML
+    GridPane gridPane;
 
     private void setUpBoard(ArrayList<TargetCard> commonTarget)
     {
@@ -49,9 +70,11 @@ public class BoardController extends GenericSceneController {
         cardOnHandBackground.setImage(myImage2);
         deckBackground.setImage(myImage3);
         pointTable.setImage(myImage4);
+        pointTable.setCursor(Cursor.HAND);
         firstTargetCard.setImage(myImage6);
         secondTargetCard.setImage(myImage7);
         backTargetCard.setImage(myImage8);
+
     }
     private void resetCardOnHand (){
         firstCardOnHand.setImage(null);
@@ -67,6 +90,7 @@ public class BoardController extends GenericSceneController {
         commonTarget=(ArrayList<TargetCard>) cp.loadTargetCards();
         setUpBoard(commonTarget);
         showDecks(goldDeck,resourceDeck);
+        setUpGridPane();
     }
 
     private void showCardOnHand(ArrayList<Card> cardOnHand){
@@ -81,10 +105,40 @@ public class BoardController extends GenericSceneController {
             }
             if(numCard==1){
                 firstCardOnHand.setImage(myImage5);
+                firstCardOnHand.setCursor(Cursor.HAND);
+                    firstCardOnHand.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            if (event.getButton() == MouseButton.PRIMARY) {
+                                isClickedCardOnHand=true;
+                            }
+                        }
+
+                    });
             }else if(numCard==2){
                 secondCardOnHand.setImage(myImage5);
+                secondCardOnHand.setCursor(Cursor.HAND);
+                secondCardOnHand.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedCardOnHand=true;
+                        }
+                    }
+
+                });
             } else if (numCard==3){
                 thirdCardOnHand.setImage(myImage5);
+                secondCardOnHand.setCursor(Cursor.HAND);
+                thirdCardOnHand.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedCardOnHand=true;
+                        }
+                    }
+
+                });
             }
             numCard++;
         }
@@ -129,16 +183,144 @@ public class BoardController extends GenericSceneController {
             }
             if(i==0){
                 firstGoldCard.setImage(image9);
+                firstGoldCard.setCursor(Cursor.HAND);
+                firstGoldCard.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedCardOnHand=true;
+                        }
+                    }
+
+                });
                 firstResourceCard.setImage(image10);
+                firstResourceCard.setCursor(Cursor.HAND);
+                firstResourceCard.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedCardOnHand=true;
+                        }
+                    }
+
+                });
+
             }else if(i==1){
                 secondGoldCard.setImage(image9);
+                secondGoldCard.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedCardOnHand=true;
+                        }
+                    }
+
+                });
                 secondResourceCard.setImage(image10);
+                secondResourceCard.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedCardOnHand=true;
+                        }
+                    }
+
+                });
             }else if(i==2){
                 kingdomGoldDeck.setImage(image9);
+                kingdomGoldDeck.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedCardOnHand=true;
+                        }
+                    }
+
+                });
                 kingdomResourceDeck.setImage(image10);
+                kingdomGoldDeck.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedCardOnHand=true;
+                        }
+                    }
+
+                });
             }
         }
     }
+    private void setUpGridPane() {
+        for(int i= 0; i<82; i++)
+            for(int j=0; j<82; j++) {
+                ImageView imageView = new ImageView();
+                imageView.setImage(new Image(getClass().getResourceAsStream("/images/view/heart.png")));
+                imageView.setCursor(Cursor.HAND);
+                imageView.setFitWidth(10);
+                imageView.setFitHeight(10);
+                imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.getButton() == MouseButton.PRIMARY) {
+                            isClickedBoard=true;
+                        }
+                    }
+
+                });
+                StackPane stackPane= new StackPane(imageView);
+                stackPane.setAlignment(Pos.CENTER);
+                gridPane.add(stackPane, i, j);
+            }
+    }
+
+    public void playACard(int indexCardOnHand, Coordinate coo,  boolean isFront){
+
+    }
+    private void clickCardOnHand(){
+        boolean ok=true, player=false;
+        for(int i = 0; i<cardsOnHand.size(); i++){
+            ImageView im=cardsOnHand.get(i);
+            im.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    if (event.getButton() == MouseButton.PRIMARY) {
+                        isClickedBoard=true;
+                    }
+                }
+
+            });
+        }
+        // please choose where to put it
+    }
+
+    private void clickBoard(){
+
+    }
+
+    private void chooseFrontOrBack(){
+
+    }
+
+    private void RemovePreviousImageview(){
+        /*
+        public Node removeNodeByRowColumnIndex(final int row,final int column,GridPane gridPane) {
+
+            ObservableList<Node> childrens = gridPane.getChildren();
+            for(Node node : childrens) {
+                if(node instanceof ImageView && gridPane.getRowIndex(node) == row && gridPane.getColumnIndex(node) == column) {
+                    ImageView imageView=ImageView(node); // use what you want to remove
+                    gridPane.getChildren().remove(imageView);
+                    break;
+                }
+            }
+        }
+        */
+    }
+
+    private void askServerIfPossiblePutCard(){
+
+    }
+
     @Override
     public void updateModel(){
         ViewModel model = getGuiApplication().getGui().getMyMatch();
